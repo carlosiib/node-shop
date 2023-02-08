822f21c7aa17b9465d366193a7d62b1c85ca81c0
carlosiib <ntic.carlos@gmail.com>
2023-02-08 13:56:03 -0700

    add fs to product model - w/ callback

--- models/product.js
+++ models/product.js
@@ -1,15 +1,33 @@
-const products = []
+const fs = require('fs')
+const path = require('path')
 
+const rootDir = require('../utils/path')
 module.exports = class Product {
   constructor(t) {
     this.title = t
   }
 
   save() {
-    products.push(this)
+    const p = path.join(rootDir, 'data', 'products.json')
+    fs.readFile(p, (error, fileContent) => {
+      let products = []
+      if (!error) {
+        products = JSON.parse(fileContent)
+      }
+      products.push(this)
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(error)
+      })
+    })
   }
 
-  static fetchAll() {
-    return products
+  static fetchAll(cb) {
+    const p = path.join(rootDir, 'data', 'products.json')
+    fs.readFile(p, (error, fileContent) => {
+      if (error) {
+        cb([])
+      }
+      cb(JSON.parse(fileContent))
+    })
   }
 }
