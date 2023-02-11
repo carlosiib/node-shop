19b3340d08dcb753e0cf3b1765efa82873f091e0
carlosiib <ntic.carlos@gmail.com>
2023-02-11 12:00:31 -0700

    cart post request w/ input hidden - 122

--- controllers/shop.js
+++ controllers/shop.js
@@ -45,6 +45,13 @@ exports.getCart = (req, res) => {
   });
 }
 
+exports.postCart = (req, res) => {
+  const { productId } = req.body
+  console.log(productId)
+  res.redirect('/cart')
+}
+
+
 exports.getOrders = (req, res) => {
   res.render('shop/orders', {
     pageTitle: 'Orders',
