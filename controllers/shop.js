120f10f1af03602de2c480d837bd28d88bae1705
carlosiib <ntic.carlos@gmail.com>
2023-02-12 15:54:44 -0700

    get cart product and render cart view

--- controllers/shop.js
+++ controllers/shop.js
@@ -40,11 +40,23 @@ exports.getIndex = (req, res) => {
 }
 
 exports.getCart = (req, res) => {
-  res.render('shop/cart', {
-    pageTitle: 'Cart',
-    path: '/cart',
+  Cart.getCart(cart => {
+    Product.fetchAll(products => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cart.products.find(prod => prod.id === product.id);
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: cartProducts
+      });
+    });
   });
-}
+};
 
 exports.postCart = async (req, res) => {
   const { productId } = req.body
--- models/cart.js
+++ models/cart.js
@@ -35,6 +35,7 @@ module.exports = class Cart {
       })
     })
   }
+
   static deleteProduct(id, productPrice) {
     fs.readFile(p, (err, fileContent) => {
       if (err) {
@@ -49,6 +50,17 @@ module.exports = class Cart {
       fs.writeFile(p, JSON.stringify(updatedCart), err => {
         console.log(err);
       });
+    });
+  }
+
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        cb(null);
+      } else {
+        cb(cart);
+      }
     });
   }
 }
