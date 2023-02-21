f6496cd94802512aadc3ad8c9690aaa127d9e140
carlosiib <ntic.carlos@gmail.com>
2023-02-20 22:28:02 -0700

    init mongodb setup

--- index.js
+++ index.js
@@ -7,74 +7,36 @@ const app = express()
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoute = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+// const adminRoute = require('./routes/admin')
+// const shopRoutes = require('./routes/shop')
 
 const errorController = require('./controllers/error')
-const sequelize = require('./utils/database')
-
-const Product = require('./models/product')
-const User = require('./models/user')
-const Cart = require('./models/cart')
-const CartItem = require('./models/cart-item')
-const Order = require('./models/order')
-const OrderItem = require('./models/order-item')
+const mongoConnect = require('./utils/database')
 
 //npm start
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(async (req, res, next) => {
-  try {
-    const user = await User.findByPk(1)
-    req.user = user
-    next()
-  } catch (error) {
-    console.log(error)
-  }
+  // try {
+  //   const user = await User.findByPk(1)
+  //   req.user = user
+  //   next()
+  // } catch (error) {
+  //   console.log(error)
+  // }
 
 })
 
-app.use('/admin', adminRoute)
-app.use(shopRoutes)
+// app.use('/admin', adminRoute)
+// app.use(shopRoutes)
 
 app.use(errorController.get404)
 
-// Relation - association
-Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" })
-User.hasMany(Product)
+async function init() {
+  const client = await mongoConnect()
+  console.log(client)
+  return app.listen(3000)
+}
+init()
 
-// 1-1
-User.hasOne(Cart)
-Cart.belongsTo(User)
-
-// n-n
-Cart.belongsToMany(Product, { through: CartItem })
-Product.belongsToMany(Cart, { through: CartItem })
-
-Order.belongsTo(User)
-User.hasMany(Order)
-Order.belongsToMany(Product, { through: OrderItem })
-
-// Sync -> creating tables IF NOT exists
-sequelize.sync({ force: false })
-  .then(res => {
-    return User.findByPk(1)
-  })
-  .then(user => {
-    if (!user) {
-      return User.create({ name: "foo", email: "foo@gmail.com" })
-    }
-    return user
-  })
-  .then(user => {
-    return user.createCart()
-  })
-  .then(() => {
-    app.listen(3000, () => {
-      console.log("App running on port 3000")
-    })
-  })
-  .catch(error => {
-    console.log(error)
-  })
--- utils/database.js
+++ utils/database.js
@@ -1,5 +1,14 @@
-const Sequelize = require('sequelize')
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
 
-const sequelize = new Sequelize('node-complete', 'root', 'root', { dialect: 'mysql', host: 'localhost' })
+const mongoConnect = async (cb) => {
+  try {
+    const client = await MongoClient.connect('mongodb://carlosiia96:BHkkdJkhrR8mmAc9@ac-8mf0rts-shard-00-00.zsc21y9.mongodb.net:27017,ac-8mf0rts-shard-00-01.zsc21y9.mongodb.net:27017,ac-8mf0rts-shard-00-02.zsc21y9.mongodb.net:27017/?ssl=true&replicaSet=atlas-pqpcdj-shard-0&authSource=admin&retryWrites=true&w=majority')
+    return client
+  } catch (error) {
+    console.log(error)
+  }
+}
 
-module.exports = sequelize
+module.exports = mongoConnect
+
