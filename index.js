3a67374cf7dc0b9deafa734595125b272b2d8f86
carlosiib <ntic.carlos@gmail.com>
2023-03-06 21:42:01 -0700

    add user mongoose model

--- index.js
+++ index.js
@@ -12,21 +12,21 @@ const adminRoute = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 const errorController = require('./controllers/error')
-// const User = require('./models/user')
+const User = require('./models/user')
 
 //npm start
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use(async (req, res, next) => {
-// try {
-//   const user = await User.findById('640039855dea820e9cb6b1a1')
-//   req.user = new User(user.name, user.email, user.cart, user._id)
-//   next()
-// } catch (error) {
-//   console.log(error)
-// }
-//})
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findById('6406c073eec6bd5a6bf0866b')
+    req.user = user
+    next()
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 app.use('/admin', adminRoute)
 app.use(shopRoutes)
@@ -36,6 +36,17 @@ app.use(errorController.get404)
 async function init() {
   try {
     await mongoose.connect('mongodb://carlosiia96:BHkkdJkhrR8mmAc9@ac-8mf0rts-shard-00-00.zsc21y9.mongodb.net:27017,ac-8mf0rts-shard-00-01.zsc21y9.mongodb.net:27017,ac-8mf0rts-shard-00-02.zsc21y9.mongodb.net:27017/?ssl=true&replicaSet=atlas-pqpcdj-shard-0&authSource=admin&retryWrites=true&w=majority')
+    const existingUser = await User.findOne()
+    if (!existingUser) {
+      const user = new User({
+        name: "foo",
+        email: "foo@gmail.com",
+        cart: {
+          items: []
+        }
+      })
+      user.save()
+    }
     return app.listen(3000)
   } catch (error) {
     console.log(error)
--- models/user.js
+++ models/user.js
@@ -1,3 +1,33 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  cart: {
+    items: [
+      {
+        productId: {
+          type: Schema.Types.ObjectId,
+          required: true
+        },
+        quantity: {
+          type: Number,
+          required: true
+        }
+      }]
+  }
+})
+
+module.exports = mongoose.model('User', UserSchema)
+
 // const mongodb = require('mongodb')
 // const getDb = require('../utils/database').getDb
 // class User {
