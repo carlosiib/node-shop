9907eae99a89ee929f1be42c1ed3961a99e516d3
carlosiib <ntic.carlos@gmail.com>
2023-03-29 12:46:12 -0700

    multer middleware storage config - 323

--- index.js
+++ index.js
@@ -17,6 +17,26 @@ const store = new MongoDBStore({
 })
 const csrfProtection = csrf()
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    // path to store image
+    cb(null, 'images')
+  },
+  filename: (req, file, cb) => {
+    // setting filename
+    const generateNumber = () => Math.floor(Math.random() * 100 + 1)
+    cb(null, `${generateNumber()}-${generateNumber()}-${file.originalname}`)
+  }
+})
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+    cb(null, true)
+  } else {
+    cb(null, false)
+  }
+}
+
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -30,7 +50,7 @@ const User = require('./models/user')
 
 //npm start
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(multer({ dest: 'images' }).single('image'))
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'))
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false, store: store }))
 app.use(csrfProtection)
