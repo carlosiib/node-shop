5c9db0efb16650780b4024626f9f6cd8175b0883
carlosiib <ntic.carlos@gmail.com>
2023-03-28 19:16:13 -0700

    add error middleware to shop controllers

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,12 +10,14 @@ exports.getProducts = async (req, res, next) => {
       path: '/products',
       isAuthenticated: req.session.isLoggedIn
     });
-  } catch (error) {
-    console.log(error)
+  } catch (err) {
+    const error = new Error("Fetch products failed")
+    error.httpStatusCode = 500
+    return next(error)
   }
 }
 
-exports.getProduct = async (req, res) => {
+exports.getProduct = async (req, res, next) => {
   try {
     const { productId } = req.params
     const product = await Product.findById(productId)
@@ -25,13 +27,15 @@ exports.getProduct = async (req, res) => {
       path: '/products',
       isAuthenticated: req.session.isLoggedIn
     })
-  } catch (error) {
-    console.log(error)
+  } catch (err) {
+    const error = new Error("Fetch products failed")
+    error.httpStatusCode = 500
+    return next(error)
   }
 
 }
 
-exports.getIndex = async (req, res) => {
+exports.getIndex = async (req, res, next) => {
   try {
     const p = await Product.find()
     res.render('shop/product-list', {
@@ -41,12 +45,14 @@ exports.getIndex = async (req, res) => {
       isAuthenticated: req.session.isLoggedIn,
       csrfToken: req.csrfToken()
     });
-  } catch (error) {
-    console.log(error)
+  } catch (err) {
+    const error = new Error("Fetch products failed")
+    error.httpStatusCode = 500
+    return next(error)
   }
 }
 
-exports.getCart = async (req, res) => {
+exports.getCart = async (req, res, next) => {
   try {
     const { cart: { items } } = await req.user.populate('cart.items.productId')
     res.render('shop/cart', {
@@ -56,33 +62,39 @@ exports.getCart = async (req, res) => {
       isAuthenticated: req.session.isLoggedIn,
       csrfToken: req.csrfToken()
     });
-  } catch (error) {
-    console.log(error)
+  } catch (err) {
+    const error = new Error("Fetching cart failed")
+    error.httpStatusCode = 500
+    return next(error)
   }
 };
 
-exports.postCart = async (req, res) => {
+exports.postCart = async (req, res, next) => {
   try {
     const { productId } = req.body
     const product = await Product.findById(productId)
     await req.user.addToCart(product)
     res.redirect('/cart')
-  } catch (error) {
-    console.log(error)
+  } catch (err) {
+    const error = new Error("Add cart failed")
+    error.httpStatusCode = 500
+    return next(error)
   }
 }
 
-exports.postCartDeleteProduct = async (req, res) => {
+exports.postCartDeleteProduct = async (req, res, next) => {
   try {
     const { productId } = req.body
     await req.user.removeFromCart(productId)
     res.redirect("/cart")
-  } catch (error) {
-    console.log(error)
+  } catch (err) {
+    const error = new Error("Delete cart product failed")
+    error.httpStatusCode = 500
+    return next(error)
   }
 }
 
-exports.postOrder = async (req, res) => {
+exports.postOrder = async (req, res, next) => {
   try {
     const { email, _id: userId } = req.user
     const { cart: { items } } = await req.user.populate('cart.items.productId')
@@ -102,12 +114,14 @@ exports.postOrder = async (req, res) => {
     await order.save()
     await req.user.clearCart()
     res.redirect('/orders')
-  } catch (error) {
-    console.log(error)
+  } catch (err) {
+    const error = new Error("Post order failed")
+    error.httpStatusCode = 500
+    return next(error)
   }
 }
 
-exports.getOrders = async (req, res) => {
+exports.getOrders = async (req, res, next) => {
   try {
     const orders = await Order.find({ 'user.userId': req.user._id })
     res.render('shop/orders', {
@@ -116,16 +130,24 @@ exports.getOrders = async (req, res) => {
       orders,
       isAuthenticated: req.session.isLoggedIn
     });
-  } catch (error) {
-    console.log(error)
+  } catch (err) {
+    const error = new Error("Getting orders failed")
+    error.httpStatusCode = 500
+    return next(error)
   }
 }
 
 
-exports.getCheckout = (req, res) => {
-  res.render('shop/checkout', {
-    pageTitle: 'Checkout',
-    path: '/cart',
-    isAuthenticated: req.session.isLoggedIn
-  });
+exports.getCheckout = (req, res, next) => {
+  try {
+    res.render('shop/checkout', {
+      pageTitle: 'Checkout',
+      path: '/cart',
+      isAuthenticated: req.session.isLoggedIn
+    });
+  } catch (err) {
+    const error = new Error("Getting checkout failed")
+    error.httpStatusCode = 500
+    return next(error)
+  }
 }
